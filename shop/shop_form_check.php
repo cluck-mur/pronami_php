e1d463c861d164e3f5a2a331c40d4c743c33d2f8
K Murakawa <ikou.katsuaki.murakawa@gmail.com>
2020-08-12 11:28:40 +0900

    テキスト　Chapter7-3まで完了

--- shop/shop_form_check.php
+++ shop/shop_form_check.php
@@ -18,9 +18,13 @@
         $address = $post['address'];
         $tel = $post['tel'];
 
+        // チェックフラグ
+        $okflg = true;
+
         if ($onamae == '') {
             // 名前が入力されていない場合
             print 'お名前が入力されていません。<br /><br />';
+            $okflg = false;
         } else {
             print 'お名前<br />';
             print $onamae;
@@ -29,6 +33,7 @@
         if (preg_match('/\A[\w\-\.]+\@[\w\-\.]+\.([a-z]+)\z/', $email) == 0) {
             // メールアドレスが入力されていない場合
             print 'メールアドレスを正確に入力してください。<br /><br />';
+            $okflg = false;
         } else {
             print 'メールアドレス<br />';
             print $email;
@@ -37,10 +42,12 @@
         if (preg_match('/\A[0-9]+\z/', $postal1) == 0) {
             // 郵便番号に半角数字以外が入力されている場合
             print '郵便番号は半角数字で入力してください。<br /><br />';
+            $okflg = false;
         }
         if (preg_match('/\A[0-9]+\z/', $postal2) == 0) {
             // 郵便番号に半角数字以外が入力されている場合
             print '郵便番号は半角数字で入力してください。<br /><br />';
+            $okflg = false;
         } else {
             print '郵便番号<br />';
             print $postal1;
@@ -50,6 +57,7 @@
         }
         if ($address == '') {
             print '住所が入力されていません。<br /><br />';
+            $okflg = false;
         } else {
             print '住所<br />';
             print $address;
@@ -57,22 +65,29 @@
         }
         if (preg_match('/\A\d{2,5}-?\d{2,5}-?\d{4,5}\z/', $tel) == 0) {
             print '電話番号を正確に入力してください。<br /><br />';
+            $okflg = false;
         } else {
             print '電話番号<br />';
             print $tel;
             print '<br /><br />';
         }
 
-        print '<form method="post" action="shop_form_done.php">';
-        print '<input type="hidden" name="onamae" value="'.$onamae.'">';
-        print '<input type="hidden" name="email" value="'.$email.'">';
-        print '<input type="hidden" name="postal1" value="'.$postal1.'">';
-        print '<input type="hidden" name="postal2" value="'.$postal2.'">';
-        print '<input type="hidden" name="address" value="'.$address.'">';
-        print '<input type="hidden" name="tel" value="'.$tel.'">';
-        print '<input type="button" onclick="history.back()" value="戻る">';
-        print '<input type="submit" value="OK"><br />';
-        print '</form>';
+        if ($okflg == true) {
+            print '<form method="post" action="shop_form_done.php">';
+            print '<input type="hidden" name="onamae" value="'.$onamae.'">';
+            print '<input type="hidden" name="email" value="'.$email.'">';
+            print '<input type="hidden" name="postal1" value="'.$postal1.'">';
+            print '<input type="hidden" name="postal2" value="'.$postal2.'">';
+            print '<input type="hidden" name="address" value="'.$address.'">';
+            print '<input type="hidden" name="tel" value="'.$tel.'">';
+            print '<input type="button" onclick="history.back()" value="戻る">';
+            print '<input type="submit" value="OK"><br />';
+            print '</form>';
+        } else {
+            print '<form>';
+            print '<input type="button" onclick="history.back()" value="戻る">';
+            print '</form>';
+        }
     ?>
 </body>
 </html>
---
+++ shop/shop_form_done.php
@@ -0,0 +1,126 @@
+<?php
+    /**
+     * セッションチェック
+     */
+    // セッション開始
+    session_start();
+    // ページを変える度に合言葉を変える
+    session_regenerate_id(true);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ろくまる農園 ご注文手続き継続</title>
+</head>
+<body>
+    <?php
+        try {
+            // 共通関数を読み込む
+            require_once("../common/common.php");
+            // 安全対策
+            $post = sanitize($_POST);
+            // 前画面での入力値を変数へ代入
+            $onamae = $post['onamae'];
+            $email = $post['email'];
+            $postal1 = $post['postal1'];
+            $postal2 = $post['postal2'];
+            $address = $post['address'];
+            $tel = $post['tel'];
+
+            //**********************************
+            // メール本文を変数に格納 (ここから)
+            $honbun = '';   // 初期化
+            $honbun .= $onamae."様\n\nこのたびはご注文ありがとうございました。\n";
+            $honbun .= "\n";
+            $honbun .= "ご注文商品\n";
+            $honbun .= "--------------\n";
+
+            $cart = $_SESSION['cart'];
+            $kazu = $_SESSION['kazu'];
+            $max = count($cart);
+
+            // データベースに接続
+            $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+            $user = 'root';
+            $password = '';
+            $dbh = new PDO($dsn, $user, $password);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            for ($i = 0; $i < $max; $i++) {
+                $sql = 'SELECT name, price FROM mst_product WHERE code=?';
+                $stmt = $dbh->prepare($sql);
+                $data[0] = $cart[$i];
+                $stmt->execute($data);
+
+                $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                $name = $rec['name'];
+                $price = $rec['price'];
+                $suryo = $kazu[$i];
+                $shokei = $price * $suryo;
+
+                $honbun .= $name." ";
+                $honbun .= $price."円 x ";
+                $honbun .= $suryo."個 = ";
+                $honbun .= $shokei."円\n";
+            }
+            // DB切断
+            $dbh = null;
+
+            $honbun .= "送料は無料です。\n";
+            $honbun .= "--------------\n";
+            $honbun .= "\n";
+            $honbun .= "代金は以下の口座にお振込みください。\n";
+            $honbun .= "ろくまる銀行 やさい支店 普通口座 1234567\n";
+            $honbun .= "\n";
+            $honbun .= "□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□\n";
+            $honbun .= "　～安心野菜のろくまる農園～\n";
+            $honbun .= "\n";
+            $honbun .= "〇〇件六丸郡六丸村 123-4\n";
+            $honbun .= "電話 090-6060-xxxx\n";
+            $honbun .= "メール info@rokumarunouen.co.jp\n";
+            $honbun .= "□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□\n";
+            //
+            // メール本文を変数に格納 (ここまで)
+            //**********************************
+            /*
+            print '<br />';
+            print nl2br($honbun);
+            */
+
+            /*
+             * お客様へメールを送信する
+             */
+            $title = 'ご注文ありがとうございます。';
+            $header = 'From: info@rokumarunouen.co.jp';
+            $honbun = html_entity_decode($honbun, ENT_QUOTES, 'UTF-8');
+            mb_language('Japanese');
+            mb_internal_encoding('UTF-8');
+            mb_send_mail($email, $title, $honbun, $header);
+
+            /*
+             * お店側へメールを送信する
+             */
+            $title = 'お客様からご注文がありました。';
+            $header = 'From: '.$email;
+            $honbun = html_entity_decode($honbun, ENT_QUOTES, 'UTF-8');
+            mb_language('Japanese');
+            mb_internal_encoding('UTF-8');
+            mb_send_mail('info@rokumarunouen.co.jp', $title, $honbun, $header);
+
+            print $onamae.'様<br />';
+            print 'ご注文ありがとうございました。<br />';
+            print $email.'にメールを送りましたのでご確認ください。<br />';
+            print $postal1.'-'.$postal2.'<br />';
+            print $address.'<br />';
+            print $tel.'<br />';
+        } catch (Exception $e) {
+            print 'ただいま障害により大変ご迷惑をお掛けしております。';
+            print $e.'<br />';
+            // 強制終了
+            exit();
+        }
+    ?>
+</body>
+</html>
